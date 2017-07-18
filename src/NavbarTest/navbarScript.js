6d34346e02aded4a69afc0ced75a6a7b43dd290d
Even Hansen <even.hansen@visma.com>
2017-07-18 08:06:19 +0200

    A lot of files

---
+++ src/NavbarTest/navbarScript.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hanseeve on 17.07.2017.
+ */
+
+function toggleDropdownMenu(){
+  document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
---
+++ src/NavbarTest2/RedRingScript.js
@@ -0,0 +1,6 @@
+/**
+ * Created by hanseeve on 17.07.2017.
+ */
+function showMenu() {
+  document.getElementById("navbar").classList.toggle("show-dropdown");
+}
